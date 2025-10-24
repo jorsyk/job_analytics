850ee8f44ca525ae4d9e0e78d56cf30d738694eb
jorsyk <trubchik36@gmail.com>
2025-10-24 11:09:00 +0300

    Create VacancyModel

--- vacancies/models.py
+++ vacancies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class VacancyModel(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    company = models.CharField(max_length=255, null=False)
+    salary_from = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
+    url = models.URLField(null=False)
+    date = models.DateField()
