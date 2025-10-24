23d3b1626715720c11725195ca860e96480afc1b
jorsyk <trubchik36@gmail.com>
2025-10-24 22:57:05 +0300

    Update VacancyModel, requirements.txt

--- vacancies/models.py
+++ vacancies/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class VacancyModel(models.Model):
-    title = models.CharField(max_length=255, null=False)
-    company = models.CharField(max_length=255, null=False)
-    salary_from = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
-    url = models.URLField(null=False)
+    title = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    salary_from = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    salary_to = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    url = models.URLField(unique=True)
     date = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} — {self.company}"
