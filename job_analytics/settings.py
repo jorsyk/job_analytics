af59af4ea80a057539c582d89fc7b9b97924bbab
jorsyk <trubchik36@gmail.com>
2025-10-24 23:21:04 +0300

    Update models.py

--- job_analytics/settings.py
+++ job_analytics/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'vacancies.apps.VacanciesConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- vacancies/models.py
+++ vacancies/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class VacancyModel(models.Model):
     title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
+    city = models.CharField(max_length=255, null=True, blank=True)
     salary_from = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     salary_to = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     url = models.URLField(unique=True)
