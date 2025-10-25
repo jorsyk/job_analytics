5ea5629840088645cf4cfb80f91adaaa7838e805
jorsyk <trubchik36@gmail.com>
2025-10-25 18:20:46 +0300

    fix urls.py

--- job_analytics/urls.py
+++ job_analytics/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from vacancies import views
 
 router = routers.DefaultRouter()
-router.register(r"vacancies", views.VacancyViewSet)
+router.register(r"api/vacancies", views.VacancyViewSet)
 
 
 urlpatterns = [
