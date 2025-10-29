5f5d9c77e8093d5df9532cf998b9fcd54b48767f
jorsyk <trubchik36@gmail.com>
2025-10-29 22:38:19 +0300

    Added calery

---
+++ job_analytics/celery.py
@@ -0,0 +1,9 @@
+
+import os
+from job_analytics.celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_analytics.settings')
+
+app = Celery('job_analytics')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
