6fb3cdfb0c7b64f06f0b19d757562229102009c5
jorsyk <trubchik36@gmail.com>
2025-11-03 20:43:57 +0300

    update update update_vacancies() in tasks.py

--- vacancies/tasks.py
+++ vacancies/tasks.py
@@ -5,5 +5,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-
-
+@shared_task
+def update_vacancies():
+    """
+    Task for parse vacancies and update database
+    """
+    try:
+        logger.info("Start updating from Celery...")
+        call_command("parse_vacancies", "Python разработчик")
+        logger.info("Updating success!!!")
+        return "Success"
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        return f"Error: {e}" 
