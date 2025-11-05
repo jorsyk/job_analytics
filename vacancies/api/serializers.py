bee5325670701ba324be3f2dcfc0ae110d42f8b0
jorsyk <trubchik36@gmail.com>
2025-11-05 23:37:38 +0300

    create AvgSalarySerializer

--- vacancies/api/serializers.py
+++ vacancies/api/serializers.py
@@ -5,3 +5,7 @@ class VacancySerializer(serializers.ModelSerializer):
     class Meta:
         model = VacancyModel
         fields = ["id", "title", "company", "city", "salary_from", "url", "date"]
+
+
+class AvgSalarySerializer(serializers.Serializer):
+    avg_salary = serializers.DecimalField(max_digits=10, decimal_places=2, llow_null=True)
