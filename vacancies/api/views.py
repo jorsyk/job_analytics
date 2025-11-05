c02b61e0b833e427216317497aa554d931cf4d70
jorsyk <trubchik36@gmail.com>
2025-11-05 23:46:04 +0300

    added comments

--- vacancies/api/views.py
+++ vacancies/api/views.py
@@ -17,10 +17,14 @@ class VacancyViewSet(viewsets.ModelViewSet):
     filterset_class = VacancyFilter
 
 
-class AvgSalaryApi(APIView):
+class AvgSalaryView(APIView):
+    """
+        return avg salary
+    """
     def get(self, request):
         avg_salary = VacancyModel.objects.aaggregate(Avg('salary_from'))['salary_from_avg']
 
+    # if db is empty
         avg = float(avg_salary) if avg_salary is not None else 0.0
 
         return Response({"avg_salary": avg_salary})
