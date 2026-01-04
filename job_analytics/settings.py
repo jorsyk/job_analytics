8beba4413e23aa725354bfacefd45f4c5025e9b7
jorsyk <trubchik36@gmail.com>
2026-01-04 13:52:23 +0300

    added celery in dc.yml

--- job_analytics/settings.py
+++ job_analytics/settings.py
@@ -149,9 +149,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = "redis://cache:6379/0"
+CELERY_RESULT_BACKEND = "redis://cache:6379/1"
 
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
