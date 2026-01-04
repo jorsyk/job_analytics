9977f8de188692bc3373bf560a7ecee09bedd1ab
jorsyk <trubchik36@gmail.com>
2026-01-04 11:44:42 +0300

    bug fix settings.py

--- job_analytics/settings.py
+++ job_analytics/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1, 0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
